f7dec656f38f5199af75a5a13c8672524157bd6a
vaishaliM01 <mvaishali0188@gmail.com>
2023-09-05 04:54:26 +0000

    removed punctuations

--- mapper.py
+++ mapper.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 import sys
+import string
 
-# get all lines from stdin
+stopwords = set(['the', 'and', 'for', 'I', 'four'])
+# checking all punctuation marks other than -
 for line in sys.stdin:
-    # remove leading and trailing whitespace
-    line = line.strip()
+    chars = [c for c in line if c not in string.punctuation.replace("-", "") ]
+    line = ''.join(chars)
+    tokens = line.split()
+    # Splitting words based on -
+    for token in tokens:
+        words = token.split("-")
+        # Checking for stopwords
+        for word in words:
+            if word not in stopwords:
+                print '%s\t%s' % (word, "1")
 
-    # split the line into words; splits on any whitespace
-    words = line.split()
-
-    # output tuples (word, 1) in tab-delimited format
-    stopwords = set(['the','and','for','I','four'])
-
-    for word in words:
-        if word not in stopwords:
-            print '%s\t%s' % (word, "1")
